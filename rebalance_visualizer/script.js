371beff59054ac82c98d67cee0c815824634a0bb
Dmitry <d.korchagin@corp.mail.ru>
2022-08-28 18:39:11 +0300

    make fillGaps function

--- rebalance_visualizer/script.js
+++ rebalance_visualizer/script.js
@@ -2,19 +2,18 @@
 THROTTLING_DATA = 10
 
 SOURCE_DATA = new Map() // ticker -> [(x1, y1), (x2, y2)]
+TIME_POINTS = new Array() // [x1, x2, x3...] из значений мапки, описанной выше
 CHARTS = new Map() // xpath_id -> Chart obj
 
-function fillMock()
-{
-    for (i = 0; i < 2; ++i)
-    {
+function fillMock() {
+    console.log("filling mock data...")
+    for (i = 0; i < 2; ++i) {
         label = "mock" + i
         ticker_data = []
         value = 5 + i * 1000
         var now = new Date();
         for (var d = new Date(2020, 0, 1); d <= now; d.setDate(d.getDate() + 5)) {
-            switch (d.getDate())
-            {
+            switch (d.getDate()) {
                 case 1:
                     value += 10
                     break
@@ -31,21 +30,51 @@ function fillMock()
                     value += 20
                 
             }
-            ticker_data.push({x: new Date(d).getTime(), y: value});
+            date = new Date(d).getTime()
+            ticker_data.push({x: date, y: value});
+            TIME_POINTS.push(date)
         }
         SOURCE_DATA.set(label, ticker_data)
     }
 }
 
+// заполним пробелы в данных последними существующими данными
+// это облегчит итерацию по данным при перебалансировке
+function fillGaps() {
+    console.log("filling gaps...")
+    TIME_POINTS = [...new Set(TIME_POINTS)]
+    TIME_POINTS.sort()
+    console.log("totally", TIME_POINTS.length, "uniq points received")
+    for (const [key, series] of SOURCE_DATA.entries()) {
+        filled_series = []
+        i = 0, j = 0
+        for (;i < TIME_POINTS.length; ++i) {
+            ts = TIME_POINTS[i]
+            if (series[j].x != ts) {
+                if (j != 0)
+                    filled_series.push({x: ts, y: series[j - 1].y})
+                else
+                    filled_series.push({x: ts, y: 0})
+            } else {
+                filled_series.push(series[j])
+                if (++j == series.length)
+                    --j // оператор подергивания. Если данные кончились - всегда берем последние
+            }
+        }
+        SOURCE_DATA.set(key, filled_series)
+    }
+}
+
 function readFiles(e, mock=null)
 {
-    if (mock)
-    {
+    console.log("cleanup data ...")
+    SOURCE_DATA = new Map()
+    TIME_POINTS = new Array()
+    if (mock) {
         fillMock()
         return
     }
-    for (let f_idx = 0; f_idx < e.target.files.length; f_idx++)
-    {
+    for (let f_idx = 0; f_idx < e.target.files.length; f_idx++) {
         var file = e.target.files[f_idx];
         var file_name = file.name
         if (!file) {
@@ -60,26 +89,25 @@ function readFiles(e, mock=null)
             var ticker_data = []
             var splitted = contents.split("\n")
             for (let i = 1; i < splitted.length; i++) {
-                if (i % THROTTLING_DATA != 0)
-                    continue
+                // if (i % THROTTLING_DATA != 0)
+                //     continue
                 var line = splitted[i].split(",")
                 var value = parseFloat(line[5])
                 if (this.FileName.includes("dollar"))
                     value *= 80
                 ticker_data.push({x: line[0], y: value})
+                TIME_POINTS.push(line[0])
             }
             ticker_data.sort()
             SOURCE_DATA.set(this.FileName, ticker_data)
-            console.log("reeding file " + this.FileName + " done.")
+            console.log("reeding file " + this.FileName + " done:", ticker_data.length, "points received")
         };
         reader.FileName = file_name
-        
         reader.readAsText(file);
     }
 }
 
-function draw(xpath_chart_id, data)
-{
+function draw(xpath_chart_id, data, title_text) {
     // data contains label -> [(x1, y1), (x2, y2)]
     console.log("drawing", xpath_chart_id)
     var series_data = []
@@ -88,16 +116,13 @@ function draw(xpath_chart_id, data)
         // console.log(randomColor)
         series_data.push({
             label: key,
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: randomColor,
-            borderWidth: 2,
+            backgroundColor:randomColor,
+            borderWidth: 0,
             hoverBackgroundColor: randomColor,
             hoverBorderColor:randomColor,
             data: value,
             pointRadius: 1,
             pointHoverRadius: 5,
-            // xAxisID: "time",
-            // yAxisId: "amount"
         })
     }
     // console.log(series_data)
@@ -116,9 +141,16 @@ function draw(xpath_chart_id, data)
             scales: {
                 x: {
                     type: 'time',
-                    // time: {
-                    //     unit: 'day'
-                    // }
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: title_text,
+                },
+                legend: {
+                    position: "right",
+                    // align: "start",
                 }
             }
         }
@@ -126,12 +158,67 @@ function draw(xpath_chart_id, data)
 
 }
 
-function visualize(e)
-{
+// cчитает какое количество бумаг каждого типа нужно иметь, чтобы портфель был сбалансированным
+// начиная с переданной даты и до конца доступных дней
+function calcBalancedTickersCount(source_data, timestamp, previous_count) {
+    // проверим что входные данные валидны для алгоритма
+    for (const [_, data] of source_data) {
+        keys = data.map(a => a.x);
+        console.assert(keys.length == TIME_POINTS.length)
+        for (i = 0; i < keys.length; ++i) {
+            console.assert(keys[i] == TIME_POINTS[i])
+        }
+    }
+    startAmount = document.getElementById('start-amount').value
+    if (previous_count)
+    {
+        startAmount = 0
+        // посчитаем сколько будет денег, когда продадим то что есть
+        for (const [key, value] of previous_count) {
+            for (i = 0; i < value.length; ++i) {
+                serie = value[i]
+                if (serie.x >= timestamp) {  // попали в запрошенную дату
+                    // количество умножаем на стоимость в этот день
+                    // уж постараемся чтобы таймстемпы у количества и сырых данных совпадали
+                    // возможным расхождением из-за дырок в данных пренебрегаем
+                    startAmount += serie.y * source_data[key][i]
+                    break
+                }
+            }
+        }
+    }
+    amountForEach = startAmount / source_data.length
+
+    countsPerTicker = new Map()
+    if (!previous_count) {
+        // простой кейс - берем первую стоимость которая есть
+        for (const [key, value] of source_data.entries()) {
+            countsPerTicker.set(key, amountForEach / value[0].y)
+        }
+    } else {
+        // кейс сложнее - нужно посчитать сколько можно купить в правильный день
+        for (const [key, value] of data.entries()) {
+            for (i = 0; i < value.length; ++i) {
+                serie = value[i]
+                if (serie.x >= timestamp) {  // попали в запрошенную дату
+                    countsPerTicker[key] = amountForEach / value[0].y
+                    break
+                }
+            }
+            
+        }
+    }
+    return countsPerTicker
+}
+
+function visualize(e) {
+    fillGaps()
     // console.log(SOURCE_DATA)
     console.log("visualization started!")
-    draw('chart-row-data', SOURCE_DATA)
-    draw('chart-row-data2', SOURCE_DATA)
+    draw('chart-row-data', SOURCE_DATA, 'price')
+    countsPerTicker = calcBalancedTickersCount(SOURCE_DATA, null, null)
+    console.log(countsPerTicker)
+    draw('chart-row-data2', countsPerTicker, 'count')
 
 }
 
