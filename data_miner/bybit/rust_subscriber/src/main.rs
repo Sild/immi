fced6613c7b238be125c6dfcd43f2c0d5f7a128c
Sild <sildtm@icloud.com>
2023-11-05 05:20:21 +0100

    add recent_trades window

--- data_miner/bybit/rust_subscriber/src/main.rs
+++ data_miner/bybit/rust_subscriber/src/main.rs
@@ -4,11 +4,14 @@ use std::sync::{Arc, Mutex};
 use std::thread::sleep;
 use std::time::Duration;
 
+use crate::market_data::TradeDetail;
+
 mod algo;
 mod fs_cache;
 mod market_data;
 mod objects;
 mod updater;
+mod helpers;
 extern crate bybit;
 const PAIRS_PATH: &str = "../routes.txt";
 const LOOP_PATH: &str = "../symbol_loops.csv";
@@ -24,18 +27,21 @@ fn main() -> Result<(), Error> {
 
     let _loops = fs_cache::read_loops(LOOP_PATH)?;
 
-    let market_data = Arc::new(Mutex::new(market_data::MarketData::new(&possible_pairs)));
+    let market_data = Arc::new(Mutex::new(market_data::MarketData::new(&possible_pairs, Duration::from_secs(30))));
 
     let _threads =
         updater::run_trades_population(&market_data, &sym_to_pair, &possible_pairs);
 
     loop {
         let rt_locked = market_data.lock().unwrap();
+        let default_trade = TradeDetail::default();
         println!(
-            "recent_trades size: total={}, filled={}, empty={}",
+            "recent_trades size: total={}, filled={}, empty={}, oldest_trade_ts={}, recent_trade_ts={},",
             rt_locked.trades_zero_cnt,
             rt_locked.trades_total_cnt - rt_locked.trades_zero_cnt,
-            rt_locked.trades_zero_cnt
+            rt_locked.trades_zero_cnt,
+            rt_locked.recent_trades.first().unwrap_or(&default_trade).ts,
+            rt_locked.recent_trades.last().unwrap_or(&default_trade).ts,
         );
         sleep(Duration::from_secs(2));
     }
--- data_miner/bybit/rust_subscriber/src/market_data.rs
+++ data_miner/bybit/rust_subscriber/src/market_data.rs
@@ -1,19 +1,23 @@
-use std::{collections::HashMap};
-
+use std::{collections::HashMap, time::Duration};
+use drain_while::*;
 use bybit::ws::response::Trade;
 
-use crate::objects;
+use crate::{objects, helpers::{cur_ts_sec}};
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 pub struct TradeDetail {
+    pub sell_symb: String,
+    pub buy_symb: String,
     pub ts: u64,
     pub price: f64,
     pub quantity: f64,
 }
 
 impl TradeDetail {
-    fn new(ts: u64, price: f64, quantity: f64) -> Self {
+    fn new(sell_symb: &str, buy_symb: &str, ts: u64, price: f64, quantity: f64) -> Self {
         TradeDetail {
+            sell_symb: sell_symb.to_string(),
+            buy_symb: buy_symb.to_string(),
             ts,
             price,
             quantity,
@@ -23,12 +27,14 @@ impl TradeDetail {
 
 pub struct MarketData {
     pub trades: HashMap<String, HashMap<String, TradeDetail>>, // sell->buy->data
+    pub recent_trades: Vec<TradeDetail>,
     pub trades_zero_cnt: usize,
     pub trades_total_cnt: usize,
+    recent_trades_window: Duration,
 }
 
 impl MarketData {
-    pub fn new(pairs: &[objects::SymbolPair]) -> Self {
+    pub fn new(pairs: &[objects::SymbolPair], recent_trades_window: Duration) -> Self {
         let mut trades: HashMap<String, HashMap<String, TradeDetail>> = HashMap::new();
         let mut size: usize = 0;
         for p in pairs.iter() {
@@ -45,8 +51,10 @@ impl MarketData {
 
         MarketData {
             trades,
+            recent_trades: Vec::default(),
             trades_zero_cnt: size,
             trades_total_cnt: size,
+            recent_trades_window,
         }
     }
     pub fn add(&mut self, trade: &Trade, sym_to_pair: &HashMap<String, objects::SymbolPair>) {
@@ -62,11 +70,18 @@ impl MarketData {
         let price = trade.p.parse::<f64>().unwrap();
         let quantity = trade.v.parse::<f64>().unwrap();
 
-        let details = TradeDetail::new(trade.T, price, quantity);
+        let details = TradeDetail::new(&sell_symb, &buy_symb, trade.T, price, quantity);
         let tmp = self.trades.entry(sell_symb.to_string()).or_default();
-        let old = tmp.insert(buy_symb.to_string(), details).unwrap();
+        let old = tmp.insert(buy_symb.to_string(), details.clone()).unwrap();
         if old.ts == 0 {
             self.trades_zero_cnt -= 1;
         }
+        self.recent_trades.push(details);
+        self.remove_old_trades();
+    }
+
+    fn remove_old_trades(&mut self) {
+        let oldest_ts = cur_ts_sec() - self.recent_trades_window.as_secs();
+        self.recent_trades.drain_while(|x| x.ts < oldest_ts);
     }
 }
